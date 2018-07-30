ac79b097b2fce0e96ff8ee9b9e36a33e73e445d3
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2018-07-30 19:33:19 +0100

    Add files via upload

---
+++ myshape.py
@@ -0,0 +1,7 @@
+#myshape.py
+
+import shape
+
+shape.colour("black")
+shape.name("square")
+shape.size("large")
---
+++ shape.py
@@ -0,0 +1,8 @@
+def colour(mycolour):
+	print("The shape is coloured ", mycolour)
+
+def name(myname):
+	print("The shape name is ", myname)
+
+def size(mysize):
+	print("The size of the shape is ", mysize)
